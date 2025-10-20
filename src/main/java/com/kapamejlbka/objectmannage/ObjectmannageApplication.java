37315e158f6ca36b80c856b84c1ecf3b34a6fb70
KAPAMEJlbKA <64539508+KAPAMEJlbKA@users.noreply.github.com>
2025-10-20 20:36:43 +0300

    Initial Commit

---
+++ src/main/java/com/kapamejlbka/objectmannage/ObjectmannageApplication.java
@@ -0,0 +1,13 @@
+package com.kapamejlbka.objectmannage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectmannageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjectmannageApplication.class, args);
+	}
+
+}
