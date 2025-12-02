4df7c67bbd7eb23ab056cfccfdb11eb4f5d4ee31
KAPAMEJlbKA <64539508+KAPAMEJlbKA@users.noreply.github.com>
2025-12-03 02:37:09 +0300

    Add fiber calculator

--- src/main/java/com/kapamejlbka/objectmanager/repository/MaterialRepository.java
+++ src/main/java/com/kapamejlbka/objectmanager/repository/MaterialRepository.java
@@ -2,6 +2,8 @@ package com.kapamejlbka.objectmanager.repository;
 
 import com.kapamejlbka.objectmanager.domain.material.Material;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface MaterialRepository extends JpaRepository<Material, Long> {
+    Optional<Material> findByCode(String code);
 }
