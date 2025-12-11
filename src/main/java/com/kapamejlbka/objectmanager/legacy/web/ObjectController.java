8ca97cb989fa02b65a1b74125ef807c9514cb5dd
KAPAMEJlbKA <64539508+KAPAMEJlbKA@users.noreply.github.com>
2025-12-11 10:50:53 +0300

    Remove unused legacy web controllers and forms

--- src/main/java/com/kapamejlbka/objectmanager/legacy/web/ObjectController.java
+++
@@ -1,7 +0,0 @@
-package com.kapamejlbka.objectmanager.legacy.web;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@RequestMapping("/objects")
-public abstract class ObjectController {
-}
